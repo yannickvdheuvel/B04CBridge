4133ceef00cb7b097a43510f7ce325d9d91568dc
yannickvdheuvel <yavdhe@outlook.com>
2026-09-05 17:04:40 +0200

    Add Android app Gradle config

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "nl.yannick.b04cbridge"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "nl.yannick.b04cbridge"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1-test"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+}
